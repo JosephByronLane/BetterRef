c819458a318d18010c6849476b60feae88745eb5
Angel Sanchez <ctm.angeledu369@gmail.com>
2024-05-30 11:01:46 -0600

    Depuracion de codigo

--- src/main2.py
+++ src/main2.py
@@ -5,6 +5,6 @@ from infinite_canvas import InfiniteCanvas
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     canvas = InfiniteCanvas()
-    canvas.show()
-    canvas.loadFromFile()
+    if canvas.loadFromFile():
+        canvas.show()
     sys.exit(app.exec_())
